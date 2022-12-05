12fbdab2afa72a367108fcfd379927eaabf1bb0c
Mathew <mathoks2012@gmail.com>
2022-12-05 01:47:34 -0800

    hggfhj

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if (matrix == undefined):
-        print()
+    if (len(matrix) == 0):
+        print("", end="")
     else:
         for i in matrix:
             for j in i:
